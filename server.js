1254a227ea4615d861e093a3cf85a4a8f62d3f16
Glthub-owner <iliajaghmaidze@gmail.com>
2023-04-21 01:11:26 +0400

    Fixed url

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-app-live.onrender.com' ",
+    origin: "https://chat-app-live.onrender.com",
     methods: ["GET", "POST"],
   },
 });
